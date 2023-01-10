d13964ab2ca83bebc1f933c369a5c22b57737077
Zicka67 <art.book@hotmail.fr>
2023-01-10 20:18:56 +0100

    Ajouts de commentaire + changement de nom de var

--- country.php
+++ country.php
@@ -2,28 +2,34 @@
 Class Country
 {
     private $countryName;
-    private $allTeams;
-
+    private $allClubs;
+    
     public function __Construct ($countryName)
     {
         $this->countryName=$countryName;
-        $this->allTeams=[];
+        $this->allClubs=[];
     }
-    public function addClubsAuPays($team)
+    
+    //************** AJOUTER DES ELEMENTS DANS UN TAB VIDE **************
+    public function addClubsDuPays($club)
     {
-        $this->allTeams[] = $team; 
+        $this->allClubs[] = $club; // On ajoute l'objet $club au tableau $allClubs[]
     }
+    
+    //************** AFFICHE LES ELEMENTS AJOUTER  **************
     public function afficherClubs()
     {
-        $result= $this->countryName." contient les équipes suivantes : <br>";
-        foreach ($this->allTeams as $club)
+        $result= $this->countryName." contient les équipes suivantes : <br>";// la variable $result contient ici le countryName de $this (l'objet courant) etc..
+        foreach ($this->allClubs as $club)// On parcours tous les elements (ici $club) de l'objet courant (ici le tab allClubs)
         {
-            $result.= $club."<br>";
+            $result.= $club."<br>";// pour chaque element ($club) du tab, on l'ajoute a la var $result
         }
         return $result."<br>";
     }
+    
+    // POUR PASSER LES INFOS DE l'OBJET COUNTRY EN STRING ( pour pouvoir l'afficher )
     public function __toString()
     {
-        return $this->countryName;
+        return $this->countryName . $this->allClubs;
     }
 }
