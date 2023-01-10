dbef10207a00e6d8cfbc0de737ad34eb713ea904
Zicka67 <art.book@hotmail.fr>
2023-01-10 20:20:25 +0100

    Ajouts de commentaires + changement de nom de var ( pour plus de facilité )

--- player.php
+++ player.php
@@ -3,65 +3,73 @@ class Player
 {
     private $lastName;
     private $firstName;
-    private $birstDate;
+    private $birthDate;
     private $nationality;
     private $clubs; // ajouter par la suite
     
-    public function __Construct($lastName, $firstName, $birstDate, nationality $nationality)
+    public function __Construct($lastName, $firstName, $birthDate, nationality $nationality)
     {
         $this->lastName = $lastName;
         $this->firstName = $firstName;
-        $this->birstDate = $birstDate;
+        $this->birthDate = $birthDate;
         $this->nationality = $nationality;
         $this->clubs = [];
     }
     
-    public function getbirstDate()
+    public function getbirthDate()
     {
-        return $this->birstDate;
+        return $this->birthDate;
     }
     
-    public function setbirstDate($birstDate): self
+    public function setbirthDate($birthDate): self
     {
-        $this->birstDate = $birstDate;
+        $this->birthDate = $birthDate;
         
         return $this;
     }
-    public function addClub($newClub)
+    //************** AJOUTER DES ELEMENTS DANS UN TAB VIDE **************
+    public function addClub($club)
     {
-        $this->clubs[] = $newClub;
+        $this->clubs[] = $club;// On ajoute l'objet $club au tableau $clubs[]
     }
     public function getClub()
     {
-        $result =  "<br>" . "Le joueur " . $this . " fait parti des clubs suivants : <br>";
-        foreach ($this->clubs as $club) {
-            $result .= $club . "<br>"; // .= (concaténer) il ajoute a la variable $result les $club du tab $clubs
+        $result =  "<br>" . "Le joueur " . $this . " fait parti des clubs suivants : <br>";// la variable $result contient  " le joueur " . $this (l'objet courant) etc..
+        foreach ($this->clubs as $club) // On parcours tous les elements (ici $club) de l'objet courant (ici le tab clubs)
+        {
+            $result .= $club . "<br>"; // .= (concaténer) pour chaque element ($club) du tab, on l'ajoute a la var $result
         }
         return $result; 
     }
-    public function getNationality()
+    public function afficherNationality()
     {
         return "Le joueur " . $this . " est de nationalité: " . $this->nationality . ".";
     }
-    // public function getAge()  //pour avoir l'âge
+    
+    // POUR PASSER LES INFOS DE l'OBJET PLAYER EN STRING ( pour pouvoir l'afficher )
+    public function __toString()
+    {
+        return $this->lastName . " " . $this->firstName . " " . $this->birthDate;
+    }
+    
+    
+}
+
+
+
+// public function getAge()  //pour avoir l'âge
+// {
+    //     $now = new DateTime();
+    //     $bd = new DateTime($this->birthDate);
+    //     $age = date_diff($now, $bd);
+    //     return $age->y;
+    // }
+    // public function getInfo()  // Pour afficher toutes les infos du joueur
     // {
-        //     $now = new DateTime();
-        //     $bd = new DateTime($this->birstDate);
-        //     $age = date_diff($now, $bd);
-        //     return $age->y;
-        // }
-        // public function getInfo()  // Pour afficher toutes les infos du joueur
-        // {
-            //     $info = $this . " est née en " . $this->birstDate . " et à la nationalité " . $this->nationality . ", " . " est un joueur de  " . $this->getAge(). " ans";
-            //     foreach ($this->clubs as $club) {
-                //         $info .= "<br>" . $club . "<br>";
-                //     }
-                //     return $info;
-                // }
-                public function __toString()
-                {
-                    return $this->lastName . " " . $this->firstName . " " . $this->birstDate;
-                }
-                
-                
-            }
+        //     $info = $this . " est née en " . $this->birthDate . " et à la nationalité " . $this->nationality . ", " . " est un joueur de  " . $this->getAge(). " ans";
+        //     foreach ($this->clubs as $club) {
+            //         $info .= "<br>" . $club . "<br>";
+            //     }
+            //     return $info;
+            // }
+            
