e11c6dc70137287586285eb4bc38c741fe181462
Zicka67 <art.book@hotmail.fr>
2023-01-10 20:18:33 +0100

    Ajouts de commentaires

--- club.php
+++ club.php
@@ -4,29 +4,33 @@ class Club
     private $clubName;
     private $country;
     private $mercatos; //ajouter par la suite
-
-
+    
+    
     public function __Construct($clubName, country $country)
     {
         $this->clubName = $clubName;
         $this->country = $country;
-        $this->country->addClubsAuPays($this);
+        $this->country->addClubsDuPays($this); //Explication
         $this->mercatos = [];
     }
-    public function addPlayer($newPlayer)
+    //************** AJOUTER DES ELEMENTS DANS UN TAB VIDE **************
+    public function addPlayer($player)
     {
-        $this->mercatos[] = $newPlayer;
+        $this->mercatos[] = $player; // On ajoute l'objet $player au tableau $mercatos[]
     }
-
-    public function getPlayers()
+    
+    //************** AFFICHE LES ELEMENTS AJOUTER  **************
+    public function afficherPlayer()
     {
-        $result =  "Le club " . $this . " est composé des joueurs suivants :<br>";
-        foreach ($this->mercatos as $players) {
-            $result .= $players . "<br>"; 
+        $result =  "Le club " . $this . " est composé des joueurs suivants :<br>";// la variable $result contient  " le club " . $this (l'objet courant) etc..
+        foreach ($this->mercatos as $player) // On parcours tous les elements (ici $player) de l'objet courant (ici le tab mercatos)
+        { 
+            $result .= $player . "<br>"; // pour chaque element ($player) du tab, on l'ajoute a la var $result
         }
         return $result . "<br>";
     }
-
+    
+    // POUR PASSER LES INFOS DE l'OBJET CLUB EN STRING ( pour pouvoir l'afficher )
     public function __toString()
     {
         return $this->clubName;
