fb3a2e3ec7e586ee3ab0f2ca605134c18825e387
Zicka67 <art.book@hotmail.fr>
2023-01-11 10:44:55 +0100

    Final ?

--- mercato.php
+++ mercato.php
@@ -5,23 +5,23 @@ class Mercato
     private $club;
     private $player;
     private $contract;
-
-
+    
+    
     public function __Construct(Club $club, Player $player, $contract)
     {
         $this->club = $club;
         $this->club->addPlayer($this);//Explication
-
+        
         $this->player = $player;
         $this->player->addClub($this);//Explication
-
+        
         $this->contract = $contract;
-
+        
     }
-
+    
     // POUR PASSER LES INFOS DE l'OBJET MERCATO EN STRING ( pour pouvoir l'afficher )
     public function __toString()
     {
-        return $this->player . " est en contract avec le club : " . $this->club . " depuis " . $this->contract . ".";
+        return $this->player . " est dans le club : " . $this->club . " en " . $this->contract . ".";
     }
 }
